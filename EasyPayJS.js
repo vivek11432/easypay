5d4fb3b466d5126d73afb955c9dc23a08152d9d0
vivekkumar11432 <vivekgari@gmail.com>
2017-01-27 02:20:15 +0530

    I Commit
    I commit

---
+++ EasyPayJS.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+
+
+
+            var scroll_pos = 0;
+            $(document).scroll(function () {
+
+                if ($(document).width() > 768) {
+                    scroll_pos = $(this).scrollTop();
+                    if (scroll_pos > 20) {
+                        $("header").css('background-color', '#6E8FD2').fadeIn(1000);
+                    } else {
+                        $("header").css('background-color', 'transparent');
+                    }
+                } else {
+                    $("header").css('background-color', '#6E8FD2').fadeIn(1000);
+                }
+            });
+
+
+            $(window).resize(function () {
+                if ($(document).width() > 768) {
+                    scroll_pos = $(this).scrollTop();
+                    if (scroll_pos > 20) {
+                        $("header").css('background-color', '#6E8FD2').fadeIn(1000);
+                    } else {
+                        $("header").css('background-color', 'transparent');
+                    }
+                }
+                else {
+                    $("header").css('background-color', '#6E8FD2').fadeIn(1000);
+                }
+            })
+
+        });
